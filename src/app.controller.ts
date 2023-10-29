deb48092677a059f7119be93fd05749619636137
choewy <choewy32@gmail.com>
2023-10-29 19:12:47 +0900

    feature/skip save log at get version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 
 import { ThrottlerName } from '@app-common/enums';
+import { SkipSaveLog } from '@app-common/metadata';
 import { HttpTrafficGuard } from '@app-core/traffic';
 
 import { AppService } from './app.service';
@@ -11,7 +12,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  // @SkipSaveLog()
+  @SkipSaveLog()
   getVersion(): string {
     return this.appService.getVersion();
   }
